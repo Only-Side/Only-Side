3ff865fb98a63b7b67b372d65ce4f5ffbf29bdfe
suzuuuuu09 <bsamu5788@gmail.com>
2024-06-06 21:55:41 +0900

    わからん

--- Only-Side/Assets/Scripts/UI/LogTexts.cs
+++ Only-Side/Assets/Scripts/UI/LogTexts.cs
@@ -5,46 +5,63 @@ using UnityEngine;
 
 public class LogTexts : MonoBehaviour
 {
-    public StoryManager storyManager;
-    public TextMeshProUGUI scriptTextObject;
-    public TextMeshProUGUI nameTextObject;
-    public int textNumber;
+    public TextMeshProUGUI scriptTextObject;     // // 原稿のテキストを表示するオブジェクト
+    public TextMeshProUGUI nameTextObject;     // 名前のテキストを表示するオブジェクト
+    public int textNumber;     // n番目
 
-    private int choiceSelectNumber;
-    private int previousChoiceSelectNumberSaveListLength;
+    private int choiceSelectNumber;     // 
+    private int previousChoiceSelectNumberSaveListLength;     // リストの長さの変更確認用
 
     private void Start()
     {
+        // 初期状態でのリストの長さを保存
         previousChoiceSelectNumberSaveListLength = 
             StoryManager.choiceSelectNumberSaveList.Count;
     }
 
     public void Update()
     {
+        // テキストが入力されているとき
         if (StoryManager.scriptTexts[textNumber] != "")
         {
+            // テキストを設定する
             scriptTextObject.text = StoryManager.scriptTexts[textNumber];
             nameTextObject.text = StoryManager.nameTexts[textNumber];
         }
+        // 選択肢が表示されているとき
         else
         {
-            if (StoryManager.choiceSelectNumberSaveList[choiceSelectNumber] == 1) 
+            // 名前のテキストを表示させない
+            nameTextObject.text = "";
+            // choiceSelectNumberがリストの範囲内かチェック
+            if (choiceSelectNumber >= 0 && choiceSelectNumber < StoryManager.choiceSelectNumberSaveList.Count)
             {
-                scriptTextObject.text = StoryManager.choiceOne[textNumber];
-            }
-            else if (StoryManager.choiceSelectNumberSaveList[choiceSelectNumber] == 2)
-            {
-                scriptTextObject.text = StoryManager.choiceTwo[textNumber];
+                // 1の選択肢が選ばれたら
+                if (StoryManager.choiceSelectNumberSaveList[choiceSelectNumber] == 1)
+                {
+                    // 1の選択肢と同じテキストに
+                    scriptTextObject.text = StoryManager.choiceOne[textNumber];
+                }
+                // 2の選択肢が選ばれたら
+                else if (StoryManager.choiceSelectNumberSaveList[choiceSelectNumber] == 2)
+                {
+                    // 2の選択肢と同じテキストに
+                    scriptTextObject.text = StoryManager.choiceTwo[textNumber];
+                }
             }
         }
+        // choiceSelectNumberSaveListのリストの長さを監視
         monitorChoiceSelectNumber();
     }
 
+    // choiceSelectNumberSaveListのリストの長さを監視
     private void monitorChoiceSelectNumber()
     {
-        if(previousChoiceSelectNumberSaveListLength != StoryManager.choiceSelectNumberSaveList.Count)
+        // choiceSelectNumberSaveListの長さが変更されたとき
+        if (previousChoiceSelectNumberSaveListLength != StoryManager.choiceSelectNumberSaveList.Count)
         {
             choiceSelectNumber = StoryManager.choiceSelectNumberSaveList.Count;
+            // 現在のリストの長さを保存
             previousChoiceSelectNumberSaveListLength = 
                 StoryManager.choiceSelectNumberSaveList.Count;
         }
