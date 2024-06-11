fefe3bc8c305a6de1327719932e7a63eac85601d
suzuuuuu09 <bsamu5788@gmail.com>
2024-06-11 18:12:48 +0900

    #65 アイテムスクリプト修正

--- Only-Side/Assets/Scripts/Manager/ItemManager.cs
+++ Only-Side/Assets/Scripts/Manager/ItemManager.cs
@@ -10,15 +10,14 @@ public class ItemManager : MonoBehaviour
 {
     public static ItemManager instance;
 
-    public GameObject itemInventoryObject;
-    public InventoryUI inventoryUI;
-    public List<int> itemNumberList;
-    public List<GameObject> spawnedPrefabSlotList = new List<GameObject>();
-    public GameObject slotPrefabObject;
-    public GameObject slotsObject;
-    public ItemDataBase itemDataBase;
+    public List<int> itemNumberList;     // 取得アイテムを格納するリスト
+    public GameObject itemInventoryObject;     // アイテムのインベントリオブジェクト
+    public GameObject slotPrefabObject;     // スロットのプレハブ
+    public GameObject slotsObject;     // スロットのプレハブの親オブジェクト
+    public ItemDataBase itemDataBase;     // アイテムのデータベース
 
-    private bool isDisplayItemInventory;
+    private List<GameObject> spawnedPrefabSlotList = new List<GameObject>();     // スロットのプレハブオブジェクトを格納するリスト
+    private bool isDisplayItemInventory;     // インベントリが見えているか
 
     private void Start()
     {
@@ -37,30 +36,21 @@ public class ItemManager : MonoBehaviour
 
     private void Update()
     {
-        SetSlots();
+        SetSlotsIcon();
     }
 
-    public void OnInventoryMenu(InputAction.CallbackContext context)
-    {
-        if(isDisplayItemInventory)
-        {
-            isDisplayItemInventory = false;
-        }
-        else
-        {
-            isDisplayItemInventory = true;
-        }
-        itemInventoryObject.SetActive(isDisplayItemInventory);
-    }
 
-    private void SetSlots()
+    // スロットのアイコンを設定
+    private void SetSlotsIcon()
     {
         while (spawnedPrefabSlotList.Count < 16)
         {
             for (int i = 0; i < 16; i++)
             {
+                // スロットのプレハブ1を6個を生成
                 GameObject _slotPrefabObject = Instantiate(slotPrefabObject, transform.position,
                     Quaternion.identity, slotsObject.transform);
+                // リストに入れる
                 spawnedPrefabSlotList.Add(_slotPrefabObject);
             }
         }
@@ -68,14 +58,37 @@ public class ItemManager : MonoBehaviour
         {
 
         }
-        int count = Mathf.Min(itemNumberList.Count, spawnedPrefabSlotList.Count);
-        for (int i = 0; i < count; i++)
+        // itemNumberListの個数かspawnedPrefabSlotListの個数のを比較して最小値をとる
+        int _count = Mathf.Min(itemNumberList.Count, spawnedPrefabSlotList.Count);
+        for (int i = 0; i < _count; i++)
         {
+            // Slotのコンポーネントを取得
             Slot slot = spawnedPrefabSlotList[i].GetComponent<Slot>();
+            // Slotのコンポーネントを取得できているか
             if (slot != null)
             {
+                // リストの画像をアイコンの画像に
                 slot.itemIconObject.sprite = itemDataBase.itemDatas[itemNumberList[i]].sprite;
             }
         }
+    }
+
+    // InputActionのInventoryMenuが押されたとき実行
+    public void OnInventoryMenu(InputAction.CallbackContext context)
+    {
+        // インベントリが見えていたら
+        if (isDisplayItemInventory)
+        {
+            // オフに
+            isDisplayItemInventory = false;
+        }
+        // インベントリが見えていないなら
+        else
+        {
+            // オンに
+            isDisplayItemInventory = true;
+        }
+        // インベントリのアクティブを設定
+        itemInventoryObject.SetActive(isDisplayItemInventory);
     }
 }
