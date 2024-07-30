df0287308a24f3555c7f670efdb0f19fb65606d7
suzuuuuu09 <bsamu5788@gmail.com>
2024-07-30 17:56:05 +0900

    update: シーン移行時のオブジェ取得(#75)

---
+++ Only-Side/Assets/Scripts/Unity/Hierarchy.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public static class Hierarchy
+{
+    // オブジェクトのヒエラルキーパスを取得するメソッド
+    public static string GetHierarchyPath(Transform obj)
+    {
+        // ルートオブジェクトに達するまで再帰的に親を辿る
+        if (obj.parent == null)
+        {
+            return "/" + obj.name;
+        }
+        return GetHierarchyPath(obj.parent) + "/" + obj.name;
+    }
+}
